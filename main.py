70670eac8804cd3b31aa578d4b0b279d6b29c371
Aman jain <prathaman111@gmail.com>
2021-09-02 11:15:46 +0530

    fix spaces

--- main.py
+++ main.py
@@ -34,28 +34,28 @@ flist = glob.glob(os.path.join(L3_SM_P_dir, '*.h5'))
 
 # Read binary files and reshape to correct size
 lats = np.fromfile('EASE2_M36km.lats.964x406x1.double', 
-					  dtype=np.float64).reshape((406,964))#< reshape to dimensions above
+						dtype=np.float64).reshape((406,964))#< reshape to dimensions above
 lons = np.fromfile('EASE2_M36km.lons.964x406x1.double', 
-					  dtype=np.float64).reshape((406,964))
+						dtype=np.float64).reshape((406,964))
 
 
 
 
 class SML3PSoilMoist:
-  """
-  get soil moisture from L3 SMAP SCA-V algo for the specified date
-  Parameters
-  ----------
-  soil_moisture: numpy.array
-  flag_id:  [str] Quality flag of retrieved soil moisture using SCA-V
-  var_id: [str] can be replaced with scva algorithm which is the default (baseline)
-  group_id: [str] retrive soil moisture for Ascending or descending pass
-  file_list: [list] of downloaded files; File path of a SMAP L3 HDF5 file
-  -------
-  Returns Soil moisture values and time period as a DataFrame
-  """
+	"""
+	get soil moisture from L3 SMAP SCA-V algo for the specified date
+	Parameters
+	----------
+	soil_moisture: numpy.array
+	flag_id:  [str] Quality flag of retrieved soil moisture using SCA-V
+	var_id: [str] can be replaced with scva algorithm which is the default (baseline)
+	group_id: [str] retrive soil moisture for Ascending or descending pass
+	file_list: [list] of downloaded files; File path of a SMAP L3 HDF5 file
+	-------
+	Returns Soil moisture values and time period as a DataFrame
+	"""
 
-  def __init__(self, file_list : 'list', orbit_pass: 'str'):
+	def __init__(self, file_list : 'list', orbit_pass: 'str'):
 	
 	self.file_list = file_list
 	self.time_period = len(self.file_list)
@@ -65,26 +65,26 @@ class SML3PSoilMoist:
 	self.var_id = 'soil_moisture'  
 	
 	if orbit_pass.upper() =='PM':
-	  self.flag_id += '_' + self.orbit_pass.lower()
-	  self.var_id += '_' + self.orbit_pass.lower()
+		self.flag_id += '_' + self.orbit_pass.lower()
+		self.var_id += '_' + self.orbit_pass.lower()
 	
 	with h5py.File(self.file_list[0], 'r') as f:
-	  self.shape_data = f[self.group_id][self.var_id].shape
+		self.shape_data = f[self.group_id][self.var_id].shape
 	
 
-  def run_(self):
+	def run_(self):
 	"""read files and return 3d array and time"""
 	times = []
 	sm_data_3d = np.empty([self.shape_data[0],self.shape_data[1],self.time_period])
 	
 	for i, fName in enumerate(self.file_list):
-	  sm_data_3d[:,:,i], time_i = self.read_SML3P(fName)
-	  times.append(time_i)
+		sm_data_3d[:,:,i], time_i = self.read_SML3P(fName)
+		times.append(time_i)
 
 	return sm_data_3d, times
 
 
-  def read_SML3P(self, filepath):
+	def read_SML3P(self, filepath):
 	''' This function extracts soil moisture from SMAP L3 P HDF5 file.
 	# refer to https://nsidc.org/support/faq/how-do-i-interpret-surface-and-quality-flag-information-level-2-and-3-passive-soil
 
@@ -116,7 +116,7 @@ class SML3PSoilMoist:
 
 	return soil_moisture, date
 
-  def generate_time_series(self, bbox: 'list -> [N_lat, S_lat, W_lon, E_lon]'):
+	def generate_time_series(self, bbox: 'list -> [N_lat, S_lat, W_lon, E_lon]'):
 	
 	N_lat, S_lat, W_lon, E_lon = bbox
 	subset = (lats<N_lat)&(lats>S_lat)&(lons>W_lon)&(lons<E_lon)
